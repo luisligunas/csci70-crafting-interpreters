f24cb0c8dee72f54bed0ca40afa0cd62374ea9b4
DreiCollanto <80865418+DreiCollanto@users.noreply.github.com>
2022-09-27 02:29:01 +0800

    chap9 added/ untested

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -1,10 +1,28 @@
 package com.craftinginterpreters.lox;
 
-class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
+
+class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+  	private Environment environment = new Environment();
+
 	@Override
 	public Object visitLiteralExpr(Expr.Literal expr) {
     	return expr.value;
   	}
+
+	@Override
+  	public Object visitLogicalExpr(Expr.Logical expr) {
+    Object left = evaluate(expr.left);
+
+    if (expr.operator.type == TokenType.OR) {
+      if (isTruthy(left)) return left;
+    } 
+	else {
+      if (!isTruthy(left)) return left;
+    }
+
+    return evaluate(expr.right);
+ 	}
 
     @Override
 	public Object visitUnaryExpr(Expr.Unary expr) {
@@ -21,6 +39,11 @@ class Interpreter implements Expr.Visitor<Object> {
     // Unreachable.
     return null;
 	}
+
+	@Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+    	return environment.get(expr.name);
+  	}
 
 	private void checkNumberOperand(Token operator, Object operand) {
     	if (operand instanceof Double) return;
@@ -70,6 +93,79 @@ class Interpreter implements Expr.Visitor<Object> {
     	return expr.accept(this);
     }
 
+    private void execute(Stmt stmt) {
+    	stmt.accept(this);
+  	}
+
+  	void executeBlock(List<Stmt> statements, Environment environment) {
+	    Environment previous = this.environment;
+	    try {
+	      this.environment = environment;
+
+	      for (Stmt statement : statements) {
+	    	execute(statement);
+	      }
+	    } finally {
+	      this.environment = previous;
+	    }
+ 	}
+
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+    	executeBlock(stmt.statements, new Environment(environment));
+    	return null;
+  	} 	
+
+    @Override
+	public Void visitExpressionStmt(Stmt.Expression stmt) {
+	    evaluate(stmt.expression);
+	    return null;
+  	}
+
+	  @Override
+  public Void visitIfStmt(Stmt.If stmt) {
+    if (isTruthy(evaluate(stmt.condition))) {
+      execute(stmt.thenBranch);
+    } else if (stmt.elseBranch != null) {
+      execute(stmt.elseBranch);
+    }
+    return null;
+  	}
+
+	@Override
+  	public Void visitPrintStmt(Stmt.Print stmt) {
+	    Object value = evaluate(stmt.expression);
+	    System.out.println(stringify(value));
+	    return null;
+	}
+
+	@Override
+	public Void visitVarStmt(Stmt.Var stmt) {
+	    Object value = null;
+	    if (stmt.initializer != null) {
+	      value = evaluate(stmt.initializer);
+	    }
+
+	    environment.define(stmt.name.lexeme, value);
+	    return null;
+	}
+
+	@Override
+	public Void visitWhileStmt(Stmt.While stmt) {
+	  while (isTruthy(evaluate(stmt.condition))) {
+		execute(stmt.body);
+	  }
+	  return null;
+	}
+
+	@Override
+  	public Object visitAssignExpr(Expr.Assign expr) {
+    	Object value = evaluate(expr.value);
+    	environment.assign(expr.name, value);
+    	return value;
+  	}
+
+
     @Override
 	public Object visitBinaryExpr(Expr.Binary expr) {
     	Object left = evaluate(expr.left);
@@ -114,14 +210,15 @@ class Interpreter implements Expr.Visitor<Object> {
 	    return null;
     }
 
-    void interpret(Expr expression) { 
+	void interpret(List<Stmt> statements) {
 	    try {
-	    	Object value = evaluate(expression);
-	    	System.out.println(stringify(value));
-	    } catch (RuntimeError error) {
-	    	Lox.runtimeError(error);
-	    }
-  	}
+		    for (Stmt statement : statements) {
+		    	execute(statement);
+		    }
+		} catch (RuntimeError error) {
+		    Lox.runtimeError(error);
+		}
+	}
 
 
 }
