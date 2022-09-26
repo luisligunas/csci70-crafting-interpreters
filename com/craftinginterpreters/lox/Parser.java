429e08ea712649d8400e61521e6526d23f611fef
Adam Zachary Bulatao <adam.bulatao@obf.ateneo.edu>
2022-09-26 21:51:41 +0800

    performed set1c
    i just switched factor and term around idk it just worked for some reason

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -217,24 +217,24 @@ class Parser {
     return expr;
   }
 
-  private Expr term() {
-    Expr expr = factor();
-
-    while (match(MINUS, PLUS)) {
-      Token operator = previous();
-      Expr right = factor();
-      expr = new Expr.Binary(expr, operator, right);
-    }
-
-    return expr;
-  }
-
   private Expr factor() {
     Expr expr = unary();
 
     while (match(SLASH, STAR)) {
       Token operator = previous();
       Expr right = unary();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr term() {
+    Expr expr = factor();
+
+    while (match(MINUS, PLUS)) {
+      Token operator = previous();
+      Expr right = factor();
       expr = new Expr.Binary(expr, operator, right);
     }
 
